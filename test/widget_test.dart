2acd3a825afa12d8f98f45fc7cd7fc1635a2f19f
Sahil Joshi <sahil140106@gmail.com>
2026-04-04 15:29:57 +0530

    added share to app feature

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nudge_app/main.dart';
+import 'package:nudge/main.dart';  // ← changed from nudge_app to nudge
 
 void main() {
   testWidgets('App launches without crashing', (WidgetTester tester) async {
